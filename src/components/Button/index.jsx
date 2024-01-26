870c8044514aace8599001c4f67079670f535a82
nemisol <vuhoainamndt@gmail.com>
2024-01-26 22:36:19 +0700

    modified login, add register page,page not found one

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,54 @@
+import { Link } from 'react-router-dom'
+
+function Button({
+  to,
+  href,
+  children,
+  isLoading,
+  primary = false,
+  outline = false,
+  wFull = false,
+  disabled = false,
+  size,
+  className: externalClassName, // Thêm prop className để nhận thêm className từ bên ngoài
+  style,
+  ...passProps
+}) {
+  let Comp = 'button'
+  const props = {
+    ...passProps
+  }
+
+  if (to) {
+    Comp = Link
+    props.to = to
+  } else if (href) {
+    Comp = 'a'
+    props.href = href
+  }
+
+  if (disabled) {
+    Object.keys(props).forEach((key) => {
+      if (key.startsWith('on') && typeof props[key] === 'function') {
+        delete props[key]
+      }
+    })
+  }
+
+  let defaultClassName = `rounded-lg text-white min-w-[100px] py-2 px-4 hover:opacity-85 border border-transparent 
+    ${primary ? 'bg-primary' : '!border-primary !text-primary '} 
+    ${wFull ? 'w-full py-4' : ''}
+    ${disabled ? 'opacity-40 hover:opacity-40 pointer-events-none' : ''}  
+    ${size === 'xl' ? 'h-full' : ''}
+    `
+
+  let className = `${defaultClassName} ${externalClassName || ''}`
+
+  return (
+    <Comp className={className} style={style} {...props}>
+      {children}
+    </Comp>
+  )
+}
+
+export default Button
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,10 +3,10 @@ import { lazy } from 'react'
 // project imports
 import MainLayout from 'layouts/MainLayout'
 import Loadable from 'components/Loadable'
+import PageNotFound from 'pages/PageNotFound'
 
 // home routing
 // const Home = Loadable(lazy(() => import('pages/Home')))
-
 
 // ===========================|| MAIN ROUTING ||=========================== //
 
@@ -19,6 +19,10 @@ const MainRoutes = {
   //     element: <Home />
   //   }
   // ]
+}
+export const NotFound = {
+  path: '*',
+  element: <PageNotFound />
 }
 
 export default MainRoutes
