21af9e4bffbc1340f0418b5a298f162e5edbfc46
GitTuanKiet <tuank1792@gmail.com>
2024-01-26 16:07:36 +0700

    fix vite-config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'styles': '/src/styles',
+      'components': '/src/components',
+      'pages': '/src/pages',
+      'utils': '/src/utils',
+      'hooks': '/src/hooks',
+      'assets': '/src/assets',
+      'config': '/src/config',
+      'services': '/src/services',
+      'constants': '/src/constants',
+      'types': '/src/types',
+      'store': '/src/store',
+      'api': '/src/api',
+      'routes': '/src/routes'
+    }
+  }
 })
