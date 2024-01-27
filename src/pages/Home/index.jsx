804ccff17870ec0eb31f3dc4b33999d731be0c29
GitTuanKiet <tuank1792@gmail.com>
2024-01-27 14:45:33 +0700

    layout-homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,9 +9,18 @@ import Banner from 'views/home/Banner'
 const Home = () => {
   return (
     <>
-      <div className="w-full h-96 bg-yellow-50 flex items-center justify-center">
-        {/* banner */}
+      {/* widget */}
+      <div className="w-full h-24 bg-red-500 flex items-center justify-center">
+
+      </div>
+      {/* banner */}
+      <div className="w-full h-96 bg-yellow-500 flex items-center justify-center px-20">
         <Banner />
+      </div>
+
+      {/* content */}
+      <div className="w-full h-auto bg-green-500 flex items-center justify-center">
+
       </div>
     </>
   )
