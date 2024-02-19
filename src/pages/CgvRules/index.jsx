0eaed0c59d3304d3accf778dd846e5d57f4460f3
vuhoainam03 <yazaonion@gmail.com>
2024-02-19 20:07:13 +0700

    add rules page

--- src/pages/CgvRules/index.jsx
+++ src/pages/CgvRules/index.jsx
@@ -1,9 +1,110 @@
-
-
 const CgvRules = () => {
   return (
-    <div>
-      <h1>CGV Rules</h1>
+    <div className="my-10 text-[#636363]">
+      <h1 className="text-center font-bold">NHỮNG QUY ĐỊNH TẠI RẠP PHIM CGV</h1>
+
+      <div>
+        <h1 className="font-bold">I. NỘI QUY PHÒNG CHIẾU</h1>
+        <ul>
+          <li>- Không quay phim, chụp ảnh.</li>
+          <li>- Tắt chuông điện thoại.</li>
+          <li>- Không hút thuốc.</li>
+          <li>- Không gây mất trật tự.</li>
+          <li>- Không nhai kẹo cao su. </li>
+          <li>- Không mang thú cưng vào rạp.</li>
+          <li>- Bảo quản tài sản cá nhân cẩn thận.</li>
+          <li>- Chỉ thức ăn và nước uống mua tại Cụm Rạp Chiếu Phim CGV Cinemas mới được phép mang vào rạp.</li>
+          <li>
+            - Không sử dụng rượu, bia, thức uống có cồn (dù với bất kỳ nồng độ cồn nào), các chất kích thích khác trong
+            khuôn viên Cụm Rạp Chiếu Phim CGV Cinemas.
+          </li>
+          <li>- Sau 22 giờ Cụm Rạp Chiếu Phim CGV Cinemas không phục vụ khách dưới 13 tuổi.</li>
+          <li>-Sau 23 giờ Cụm Rạp Chiếu Phim CGV Cinemas không phục vụ khách dưới 16 tuổi.</li>
+          <li>
+            - Ban Quản Lý Cụm Rạp Chiếu Phim CGV Cinemas có quyền từ chối không cho Quý Khách vào Rạp Chiếu Phim nếu vi
+            phạm.
+          </li>
+          <li>- CGV Việt Nam có camera an ninh trong các Cụm Rạp Chiếu Phim CGV Cinemas.</li>
+        </ul>
+      </div>
+
+      <div>
+        <h1 className="font-bold">II. PHÂN LOẠI PHIM THEO ĐỘ TUỔI</h1>
+        <ul className="list-decimal">
+          <li>
+            Phân loại Phim
+            <p>CGV Việt Nam thông báo tiêu chí phân loại phim theo lứa tuổi như sau:</p>
+            <table className="w-full text-sm text-left rtl:text-right mt-10 text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th className="px-6 py-3">Phân loại *</th>
+                  <th className="px-6 py-3">Định nghĩa</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td>P</td>
+                  <td>Phim được phép phổ biến đến người xem ở mọi độ tuổi.</td>
+                </tr>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td>K</td>
+                  <td>Phim được phổ biến đến người xem dưới 13 tuổi và có người bảo hộ đi kèm.</td>
+                </tr>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td>T18</td>
+                  <td>Phim được phổ biến đến người xem từ đủ 18 tuổi trở lên (18+).</td>
+                </tr>
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td>C</td>
+                  <td>Phim không được phép phổ biến.</td>
+                </tr>
+              </tbody>
+            </table>
+          </li>
+          <li>
+            Lưu ý
+            <p>
+              - Quý Khách Hàng xem phim được phân loại T13, T16, T18 vui lòng mang theo giấy tờ tùy thân có ảnh nhận
+              diện và ngày tháng năm sinh để đảm bảo việc tuân thủ theo quy định. CGV Việt Nam có quyền yêu cầu khách
+              hàng xuất trình Giấy khai sinh, Căn cước công dân, Thẻ học sinh, thẻ sinh viên, bằng lái xe, hoặc các giấy
+              tờ tùy thân khác để xác định độ tuổi Quý Khách Hàng. - Ban Quản Lý Cụm Rạp Chiếu Phim CGV Cinemas có quyền
+              kiểm tra và từ chối khách hàng nếu không đúng quy định về độ tuổi.
+            </p>
+          </li>
+          <li>
+            Lưu ý
+            <p>
+              - Phạt tiền từ 60.000.000 đồng đến 80.000.000 đồng đối với hành vi không đảm bảo người xem phim đúng độ
+              tuổi theo phân loại phim.
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1 className="font-bold">III. QUY ĐỊNH VỀ KHUNG GIỜ CHIẾU PHIM CHO TRẺ EM</h1>
+        <ul className="list-decimal">
+          <li>
+            Quy định về Khung giờ chiếu phim cho trẻ em
+            <p>
+              CGV Việt Nam thông báo áp dụng quy định về khung giờ chiếu phim cho trẻ em như sau: (i) Giờ chiếu phim cho
+              trẻ em dưới 13 tuổi tại Cụm Rạp Chiếu Phim CGV Cinemas kết thúc trước 22 giờ. (ii) Giờ chiếu phim cho trẻ
+              em dưới 16 tuổi tại Cụm Rạp Chiếu Phim CGV Cinemas kết thúc trước 23 giờ.
+            </p>
+          </li>
+          <li>
+            Lưu ý
+            <p>
+              - CGV Việt Nam có quyền yêu cầu khách hàng xuất trình Giấy khai sinh, Căn cước công dân, Thẻ học sinh, thẻ
+              sinh viên, bằng lái xe, hoặc các giấy tờ tùy thân khác để xác định độ tuổi Quý Khách Hàng. - Ban Quản Lý
+              Cụm Rạp Chiếu Phim CGV Cinemas có quyền kiểm tra và từ chối khách hàng nếu không đúng quy định về độ tuổi.
+            </p>
+          </li>
+          <li>
+            Chế tài
+            <p>- Phạt tiền từ 40.000.000 đồng đến 60.000.000 đồng đối với hành vi vi phạm quy định.</p>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
