1897191dc9575f6d6c651a7e38078483305f38fc
GitTuanKiet <tuank1792@gmail.com>
2024-03-08 10:11:28 +0700

    TuanKiet - fix router return_url env file

--- src/pages/Congratulation/index.jsx
+++ src/pages/Congratulation/index.jsx
@@ -1,6 +1,46 @@
+import { useEffect } from 'react'
+
+import OrderApi from 'apis/orderApi'
+import { toast } from 'react-toastify'
+
+
 import SucceededImage from 'assets/images/image_succeeded.png'
 import Button from 'components/Button'
+
 function Congratulation() {
+
+  useEffect(() => {
+    if (window.location.search) {
+      const search = new URLSearchParams(window.location.search)
+      const token = search.get('token')
+      const PayerID = search.get('PayerID')
+      if (token) {
+        if (PayerID) {
+          OrderApi.checkOrder('paypal', token)
+            .then((res) => {
+              if (res.status === 200) {
+                toast.success(res.data.message)
+              }
+            })
+            .catch((err) => {
+              toast.error(err.response.data.message)
+            })
+        } else {
+          OrderApi.cancelOrder('paypal', token)
+            .then((res) => {
+              if (res.status === 200) {
+                toast.info(res.data.message)
+              }
+            })
+            .catch((err) => {
+              toast.error(err.response.data.message)
+            })
+        }
+      }
+      window.history.replaceState({}, document.title, window.location.pathname)
+    }
+  }, [])
+
   return (
     <div className="mx-auto text-center my-10">
       <img src={SucceededImage} alt="" className="w-full " />
