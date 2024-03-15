d63410fe8b519fe19d0e095de3038294e0e6aa96
GitTuanKiet <152750556+GitTuanKiet@users.noreply.github.com>
2024-03-15 08:27:33 +0700

    fix responsive (#24)

--- src/views/movie/MovieList/index.jsx
+++ src/views/movie/MovieList/index.jsx
@@ -1,21 +1,17 @@
 import { useSelector } from 'react-redux'
 import MovieItem from '../MovieItem'
-import imagePaths from 'assets/images/imagePaths';
+import imagePaths from 'assets/images/imagePaths'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import Heading from 'components/Heading'
 
 function MovieList() {
   const { movies } = useSelector((state) => state.data)
   return (
     <div>
-    <Heading>Movie selection</Heading>
-// =======
-//       <div className='my-10'>
-//         <hr className='border-t-2 border-black mb-[-18px]' />
-//         <img src={imagePaths.movie_select} alt="" className='mx-auto ' />
-//         <hr className='border-t-2 border-black mt-[-18px]' />
-//       </div>
-// >>>>>>> main
+      <div className='pb-10 flex justify-center items-center'>
+        <div className='border-y-2 border-black pt-1 flex-grow' ></div>
+        <img src={imagePaths.movie_select} alt="" className='px-4' />
+        <div className='border-y-2 border-black pt-1 flex-grow'></div>
+      </div>
       <div className="movie-list ">
         <Swiper grabCursor={true} spaceBetween={40} slidesPerView={'auto'}>
           {movies.map((movie) => {
